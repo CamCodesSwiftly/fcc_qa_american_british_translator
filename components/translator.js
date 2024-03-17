dfdcb10b7961627ad4c1f8b924d77e086ee51616
Kamran Babazadeh <99259950+CamCodesSwiftly@users.noreply.github.com>
2024-03-17 21:14:00 +0100

    feat: Translate from American to British
    The core functionality is ready.
    Replacing was fairly easy. But there is the problem of cases.
    The replacing lists are all in lowercase, but you have to output the same case as the input.
    I implemented a solution for the titles, which works.
    What you do is use case insensitive regex to match without manipulating anything.
    Then you replace. And when you replace, you just Uppercase the first character.

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,36 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+	translateAtoB(text) {
+		let translation = text;
+		for (let key in americanOnly) {
+			translation = translation.replaceAll(key, americanOnly[key]);
+		}
+		for (let key in americanToBritishSpelling) {
+			translation = translation.replaceAll(
+				key,
+				americanToBritishSpelling[key]
+			);
+		}
+		for (let key in americanToBritishTitles) {
+            // match titles case insensitively and then replace the title, but in Uppercase
+			const pattern = key;
+			const regex = new RegExp(pattern, "gi");
+            
+			let title = americanToBritishTitles[key];
+			let upperCaseStartingCharacter = title[0].toUpperCase();
+			let restOfTitle = americanToBritishTitles[key].substring(1);
+			let upperCasedTitle = upperCaseStartingCharacter + restOfTitle;
 
+			translation = translation.replaceAll(regex, upperCasedTitle);
+		}
+
+		return translation;
+	}
+	translateBtoA(text) {}
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,15 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
+	const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+	app.route("/api/translate").post((req, res) => {
+		let text = req.body.text;
+		let locale = req.body.locale;
+
+		let translation = translator.translateAtoB(text);
+		console.log(translation);
+	});
 };
